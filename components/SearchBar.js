84a3b74affe67c931f143084f0a8ba55c421c9b8
abhiram11 <abhiramsatpute@gmail.com>
2021-09-21 05:39:56 +0530

    added search bar with icons left and right, places api not integrated yet

---
+++ components/SearchBar.js
@@ -0,0 +1,58 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import AntDesign from "react-native-vector-icons/AntDesign";
+
+export default function SearchBar() {
+  return (
+    <View style={{ marginTop: 15, flexDirection: "row" }}>
+      <GooglePlacesAutocomplete
+        placeholder="Search"
+        styles={{
+          textInput: {
+            backgroundColor: "#eee",
+            borderRadius: 20,
+            fontWeight: "700",
+            marginTop: 7,
+          },
+
+          textInputContainer: {
+            backgroundColor: "#eee",
+            borderRadius: 50,
+            flexDirection: "row",
+            alignItems: "center",
+            marginRight: 10,
+          },
+        }}
+        //adding icons here itself! THATS WHY YOU READ DOCS!
+
+        renderLeftButton={() => (
+          <View style={{ marginLeft: 10 }}>
+            <Ionicons name="location-sharp" size={24} />
+          </View>
+        )}
+        renderRightButton={() => (
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              marginRight: 8,
+              backgroundColor: "white",
+              padding: 9,
+              borderRadius: 30,
+            }}
+          >
+            <AntDesign
+              name="clockcircle"
+              size={11}
+              style={{ marginRight: 6 }}
+            />
+            <Text>Search</Text>
+          </View>
+        )}
+      />
+      {/* <Text>SEarchbar!</Text> */}
+    </View>
+  );
+}
